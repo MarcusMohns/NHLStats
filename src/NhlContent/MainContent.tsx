1e63fbc54bb9fc0d3603707b5b3326482e89592e
Marcus Mohns <marcus@mohns.se>
2025-03-19 17:15:44 +0100

    add modal

--- src/NhlContent/MainContent.tsx
+++ src/NhlContent/MainContent.tsx
@@ -1,10 +1,20 @@
 import Standings from "./sections/standings/Standings.tsx";
 import Leaderboard from "./sections/Leaderboard.tsx";
 import UpcomingMatches from "./sections/UpcomingMatches.tsx";
+import { useState } from "react";
+import Modal from "./components/Modal.tsx";
 
 const MainContent = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const handleModalOpen = () => {
+    setModalOpen((prevState) => !prevState);
+  };
+
   return (
     <main className="flex flex-col justify-center md:flex-row w-full md:w-3/4 gap-4 mt-20">
+      {modalOpen && <Modal handleModalOpen={handleModalOpen} />}
+      <button onClick={() => setModalOpen(true)}>OPEN MODAL! :)</button>
       <Leaderboard />
       <Standings />
       <UpcomingMatches />
