0468ea8d34c27647f49a9863caa97050e21cc507
Marcus Mohns <marcus@mohns.se>
2025-03-20 22:20:31 +0100

    fix layout weirdness

--- src/NhlContent/MainContent.tsx
+++ src/NhlContent/MainContent.tsx
@@ -1,20 +1,10 @@
 import Standings from "./sections/standings/Standings.tsx";
 import Leaderboard from "./sections/Leaderboard.tsx";
 import UpcomingMatches from "./sections/UpcomingMatches.tsx";
-import { useState } from "react";
-import Modal from "./components/Modal.tsx";
 
 const MainContent = () => {
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-
-  const handleModalOpen = () => {
-    setModalOpen((prevState) => !prevState);
-  };
-
   return (
-    <main className="flex flex-col justify-center md:flex-row w-full md:w-3/4 gap-4 mt-20">
-      {modalOpen && <Modal handleModalOpen={handleModalOpen} />}
-      <button onClick={() => setModalOpen(true)}>OPEN MODAL! :)</button>
+    <main className="flex flex-col justify-center align-center lg:flex-row w-full">
       <Leaderboard />
       <Standings />
       <UpcomingMatches />
