f7a57dd60482b76d84dc63def77c5434533c6a9c
Marcus Mohns <marcus@mohns.se>
2025-03-27 18:02:18 +0100

    dark mode functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import MainContent from "./NhlContent/MainContent.tsx";
+import { useState } from "react";
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className="flex justify-center align-center">
+    <div className={`${darkMode && "dark"} flex justify-center align-center`}>
       {/* <Navbar /> */}
+      <button
+        onClick={() => setDarkMode((prevState) => !prevState)}
+        className="h-12 w-12 bg-slate-400"
+      >
+        Toggle dark mode
+      </button>
       <MainContent />
       {/* <Footer /> */}
     </div>
