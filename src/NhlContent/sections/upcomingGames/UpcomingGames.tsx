fcda78391f5c7dc6d455924fcbbd2a71496ab705
Marcus Mohns <marcus@mohns.se>
2025-04-04 17:05:02 +0200

    styles & additional functionality

--- src/NhlContent/sections/upcomingGames/UpcomingGames.tsx
+++ src/NhlContent/sections/upcomingGames/UpcomingGames.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import fetchSchedule from "../../api/fetchSchedule";
-
+import fetchUpcomingGames from "../../api/fetchUpcomingGames";
+import ErrorWithBtn from "../../components/ErrorWithBtn";
+import { ErrorType } from "../standings/Standings";
+import { linkOutIcon, spinner } from "../../components/svgs";
 type TeamType = {
   id: number;
   commonName: { default: string; [lang: string]: string };
@@ -67,6 +69,13 @@ type GameWeek = {
 const UpcomingGames = () => {
   const [upcomingGames, setUpcomingGames] = useState<null | GameWeek[]>(null);
 
+  const [error, setError] = useState<ErrorType>({
+    error: false,
+    text: "",
+    message: "",
+    name: "",
+  });
+
   const locale = navigator.language;
   // gets the users locale to format the date correctly
 
@@ -80,67 +89,108 @@ const UpcomingGames = () => {
     "Saturday",
   ];
 
+  const fetchAndSetUpcomingGames = async () => {
+    try {
+      const upcomingGamesData = await fetchUpcomingGames();
+      if (!upcomingGamesData) {
+        throw new Error("No Upcoming Games data");
+      } else {
+        setUpcomingGames(upcomingGamesData);
+        setError({ error: false, text: "", message: "", name: "" });
+      }
+    } catch (e) {
+      !error.error &&
+        setError({
+          error: true,
+          text: "Something went wrong setting Upcoming Games 🙁",
+          message: (e as Error).message,
+          name: "fetchAndSetUpcomingGames",
+        });
+      throw e;
+    }
+  };
+
   useEffect(() => {
-    fetchSchedule().then((schedule) => {
-      setUpcomingGames(schedule);
-    });
+    fetchAndSetUpcomingGames();
   }, []);
 
   let lastRenderedWeekday: string | null = null;
 
-  return (
-    <section className="UpcomingGames">
-      <div className="flex flex-col">
-        <h2 className="text-2xl">UpcomingGames</h2>
+  if (error.error) {
+    return <ErrorWithBtn error={error} action={fetchAndSetUpcomingGames} />;
+  }
 
-        {upcomingGames ? (
-          upcomingGames.map((gameDay) => (
-            <div key={gameDay.date}>
-              {gameDay.games.map((game) => {
-                if (game.gameOutcome) {
-                  return null;
-                }
-                const date = new Date(game.startTimeUTC);
-                const weekday = weekDays[date.getDay()];
-                // New Date calculates and takes into account the users timezone and daylight savings time (DST).
-                const formattedDateTime = new Intl.DateTimeFormat(
-                  locale || "sv-SE",
-                  {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  }
-                ).format(date);
-                return (
-                  <div key={game.id} className="flex flex-col">
-                    {weekday !== lastRenderedWeekday && (
-                      // Since New Date has corrected for timezones and DST, only render the date if it is the first game of the day
-                      <h2 className="text-2xl">
-                        {" "}
-                        {weekDays[date.getDay()]} - {date.getDate()}/
-                        {date.getMonth() + 1}/{date.getFullYear()}{" "}
-                      </h2>
-                    )}
-                    <div className="flex flex-row">
-                      <p className="flex flex-row">
-                        {game.awayTeam.abbrev}
-                        <img className="w-12" src={game.awayTeam.logo} />
-                      </p>
-                      <p>{formattedDateTime}</p>
-                      <p className="flex flex-row">
-                        {game.homeTeam.abbrev}
-                        <img className="w-12" src={game.homeTeam.darkLogo} />
-                      </p>
-                    </div>
-                    {(lastRenderedWeekday = weekday)}
-                  </div>
-                );
-              })}
-            </div>
-          ))
-        ) : (
-          <div>loading</div>
-        )}
+  if (!upcomingGames) {
+    return (
+      <div className="relative w-full 2xl:w-1/5 2xl:mx-10 xl:w-2/5 rounded p-3 h-235 bg-gray-300 dark:bg-stone-700 animate-pulse mt-20">
+        {spinner}
       </div>
+    );
+  }
+
+  return (
+    <section className="upcoming-games 2xl:border border-gray-300 dark:border-stone-600 shadow-md rounded p-3 w-full 2xl:w-1/5 2xl:mx-10 xl:w-2/5 h-max  mt-20">
+      <h2 className="font-bold dark:text-stone-300 mt-5 py-1 px-1 text-2xl uppercase leading-tight tracking-wide">
+        Upcoming Games
+      </h2>
+      {upcomingGames.map((gameDay) => (
+        <div key={gameDay.date}>
+          {gameDay.games.map((game) => {
+            if (game.gameOutcome) {
+              return null;
+            }
+            const date = new Date(game.startTimeUTC);
+            const weekday = weekDays[date.getDay()];
+
+            // New Date calculates and takes into account the users timezone and daylight savings time (DST).
+            const formattedDateTime = new Intl.DateTimeFormat(
+              locale || "sv-SE",
+              {
+                hour: "2-digit",
+                minute: "2-digit",
+              }
+            ).format(date);
+            return (
+              <div key={game.id} className="flex flex-col">
+                {weekday !== lastRenderedWeekday && (
+                  // Since New Date recalculates time and sometimes day we can't use the Weekday provided in API
+                  // check weekDay instead and render a H2 if it's a different day from last iteration
+                  <div className="flex text-lg flex-row w-full mt-4 py-2 font-bold dark:text-stone-300 uppercase leading-tight tracking-wide border-b border-gray-300 dark:border-stone-600">
+                    <h2>{(lastRenderedWeekday = weekday)}</h2>
+                    <p className="flex text-center align-end justify-end">
+                      ({date.getDate()}/{date.getMonth() + 1}/
+                      {date.getFullYear()})
+                    </p>
+                  </div>
+                )}
+                <div className="flex flex-row align-center justify-center shadow-sm bg-white dark:bg-stone-800 dark:shadow-stone-800 p-2 mt-1 rounded">
+                  <p className="flex items-center bg-stone-200 text-sm font-bold px-1 dark:bg-stone-700 rounded w-min ">
+                    {formattedDateTime}
+                  </p>
+                  <div className="flex flex-row items-center justify-center align-center dark:text-stone-300  text-stone-800 w-full flex-space-between font-bold">
+                    <p className="flex flex-row items-center justify-center min-w-20">
+                      {game.awayTeam.abbrev}
+                      <img className="w-8 h-8" src={game.awayTeam.logo} />
+                    </p>
+                    <p className="w-5">vs</p>
+                    <p className="flex flex-row items-center justify-center min-w-20">
+                      <img className="w-8 h-8" src={game.homeTeam.darkLogo} />
+                      {game.homeTeam.abbrev}
+                    </p>
+                    <a
+                      href={`https://www.nhl.com${game.gameCenterLink}`}
+                      className="flex items-center h-full"
+                      target="_blank"
+                    >
+                      {linkOutIcon}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ))}
     </section>
   );
 };
