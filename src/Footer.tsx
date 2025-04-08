89492e4054424a1220ec010776bf3bb15ab50f76
Marcus Mohns <marcus@mohns.se>
2025-04-08 23:16:26 +0200

    add footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-50 w-full flex flex-direction:row content-center items-center justify-center bg-stone-200 dark:bg-stone-900 dark:text-white border-b border-stone-300 dark:border-stone-900">
+      <p className="text-sm font-bold leading-tight tracking-wide">
+        Made by Marcus Mohns using React, TypeScript & Tailwindcss - 2025
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
