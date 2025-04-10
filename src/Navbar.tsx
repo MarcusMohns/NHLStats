a014babf5916b40b8d78f5ace2b5d6462d71d821
Marcus Mohns <marcus@mohns.se>
2025-04-10 20:11:21 +0200

    styles

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -8,11 +8,11 @@ type NavbarProps = {
 const Navbar = ({ toggleDarkMode, darkMode }: NavbarProps) => {
   return (
     <div
-      className="flex w-full flex-direction:row align-center items-center p-4 border-b border-stone-300 dark:border-stone-900 
+      className="flex w-full flex-direction:row align-center items-center p-4  
     bg-gradient-to-r from-stone-0 via-stone-200 to-stone-300 dark:bg-gradient-to-r dark:from-stone-700 dark:via-stone-800 dark:to-stone-900"
     >
       <img src="/images/hockey_puck.png" className="w-14 h-14" />
-      <h1 className="ml-2 content-center dark:text-white text-3xl leading-tight tracking-wide font-bold">
+      <h1 className="ml-2 content-center dark:text-white text-3xl leading-tight tracking-wide font-bold select-none">
         NHL Stats
       </h1>
       <div
