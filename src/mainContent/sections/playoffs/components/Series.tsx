64d1a2fb5dabd625e902b041f4a41cbb52d4a9d9
Marcus Mohns <marcus@mohns.se>
2025-05-07 14:35:51 +0200

    add link to series

--- src/mainContent/sections/playoffs/components/Series.tsx
+++ src/mainContent/sections/playoffs/components/Series.tsx
@@ -4,6 +4,9 @@ import Scoreboard from "./Scoreboard";
 const GameSeries = ({ series }: { series: SeriesType }) => {
   return (
     <div className={`flex flex-col text-center justify-center m-1 md:m-2`}>
+      <p className="font-bold dark:text-stone-300 leading-tight tracking-wide select-none text-sm">
+        {series.seriesAbbrev}
+      </p>
       {series.seriesUrl && series.bottomSeedTeam && series.topSeedTeam ? (
         <Scoreboard
           awayTeam={series.bottomSeedTeam}
@@ -11,6 +14,7 @@ const GameSeries = ({ series }: { series: SeriesType }) => {
           awayTeamScore={series.bottomSeedWins}
           homeTeamScore={series.topSeedWins}
           winningTeamId={series.winningTeamId}
+          url={series.seriesUrl}
         />
       ) : series.seriesLogo ? (
         <div className="border-2 p-2 rounded border-gray-300 dark:border-stone-700">
