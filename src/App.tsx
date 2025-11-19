0420556634405abb4fe4bde8ce5ae8296ee3b66e
Marcus Mohns <marcus@mohns.se>
2025-11-19 14:55:58 +0100

    refractor dark mode into custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import MainContent from "./mainContent/MainContent.tsx";
 import Navbar from "./Navbar.tsx";
 import Footer from "./Footer.tsx";
-import { useState } from "react";
-import startViewTransitionWrapper from "./utility/startViewTransitionWrapper.ts";
+import useDarkmode from "./hooks/useDarkMode.ts";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
-    // Initialize dark mode state based on localStorage or system preference
-    const storedDarkMode = localStorage.getItem("darkMode");
-    // If theres a stored boolean for dark mode use it
-    if (storedDarkMode) return true;
-    else if (window.matchMedia("(prefers-color-scheme: dark)").matches)
-      // Try to use the system preference
-      return true;
-    // Default to light mode
-    else return false;
-  });
-
-  // Toggle dark mode and persist the preference in localStorage
-  const toggleDarkMode = () => {
-    const newDarkModeState = !isDarkMode;
-    localStorage.setItem("darkMode", JSON.stringify(newDarkModeState));
-    startViewTransitionWrapper(() => setIsDarkMode(newDarkModeState));
-  };
+  const { isDarkMode, toggleDarkMode } = useDarkmode();
 
   return (
     <div className={`${isDarkMode ? "dark" : ""} dark:bg-stone-900`}>
